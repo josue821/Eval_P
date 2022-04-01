bd175ebda7ed42fcd5dfda4cfa1d23b64b7d82b9
cazagajo <josuepoolchable@gmail.com>
2022-03-31 23:54:31 -0600

    Primer version

---
+++ app/src/main/java/com/example/evalp/vistas/cuestionario.java
@@ -0,0 +1,16 @@
+package com.example.evalp.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.evalp.R;
+
+public class cuestionario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuestionario);
+    }
+}
